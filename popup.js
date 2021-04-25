a2b0c55456cbef44146d3b5dd271c985591d1c0a
villr10 <59181636+villr10@users.noreply.github.com>
2021-04-25 01:43:11 -0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -5,6 +5,23 @@ window.onload = function(){
 document.getElementById("checkPage").addEventListener("click", myFunction);
 document.getElementById("checkTenant").addEventListener("click", checkTenantAPI);
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
 function myFunction() {
   var textbox = document.getElementById("tid").value;
   var textboxsanatized = textbox.trim();
